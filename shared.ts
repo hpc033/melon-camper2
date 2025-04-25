07fe8abff561e67299d1bb172ea6cf4952159cf5
hpc033 <33014422+hpc033@users.noreply.github.com>
2025-04-25 14:14:14 +0800

    Update shared.ts

--- shared.ts
+++ shared.ts
@@ -20,7 +20,7 @@ export const LOGIN_URL: string =
  * The link to your target concert, on the Melon Global website.
  */
 export const CONCERT_URL =
-  "https://tkglobal.melon.com/performance/index.htm?langCd=EN&prodId=211212"
+  "https://tkglobal.melon.com/performance/index.htm?langCd=EN&prodId=211290"
 
 /**
  * Sometimes, concerts run over multiple days.
